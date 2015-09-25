58764009f2a71b6ae012d0fb9f8c73ce1c52078b
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2015-09-25 13:23:39 -0300

    Insere minificarjs.js

---
+++ gulp/tasks/minificarjs.js
@@ -0,0 +1,17 @@
+// plugins
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// tasks
+gulp.task('minificarjs', function() {
+	return gulp.src('../assets/js/*.js')
+		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('../assets/js-min'))
+
+});
+
+gulp.task('default', function(){
+	gulp.run('minificarjs')
+});
