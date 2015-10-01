b8c0a003da5ce50cd575fc2575b4e9af52d32cc1
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2015-10-01 14:02:28 -0300

    Renomeia compile-js.js

---
+++ gulp/tasks/compile-js.js
@@ -0,0 +1,13 @@
+// plugins
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// tasks
+gulp.task('compile-js', function() {
+	return gulp.src('../assets/src/js/*.js')
+		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('../assets/public/js-min'))
+
+});
