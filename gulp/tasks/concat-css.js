04a78d4a26a6195bac6c1efca2c8a43b2c830d4c
Anderson <theandersonn@gmail.com>
2015-10-04 11:09:50 -0300

    Renomeia concat-files > concat-css

--- gulp/tasks/concat-css.js
+++ gulp/tasks/concat-css.js
@@ -6,7 +6,12 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 
 gulp.task('concat-css', function() {
-  return gulp.src(['../assets/public/css/reset.min.css', '../assets/public/css/grid.min.css', '../assets/public/css/base.min.css', '../assets/public/css/media-queries.min.css'])
+  return gulp.src([
+      '../assets/public/css/reset.min.css',
+      '../assets/public/css/grid.min.css',
+      '../assets/public/css/base.min.css',
+      '../assets/public/css/media-queries.min.css'
+  ])
     .pipe(concat('all.min.css'))
-    .pipe(gulp.dest('../assets/public/css-min/'));
+    .pipe(gulp.dest('../assets/public/css/'));
 });
