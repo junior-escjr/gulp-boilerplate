31b095e91015444784b2cb2496ec09b987c5eb72
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2015-11-03 13:49:26 -0200

    Ajusta campo de dominio da pasta sass e watch

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -6,7 +6,7 @@ var gulp = require('gulp');
 
 gulp.task('default', ['jsmin', 'sass', 'cssmin', 'concatcss'], function(){
 	gulp.watch('../assets/src/js/*.js', ['jsmin']);
-    gulp.watch('../assets/src/sass/*.scss', ['sass']);
+    gulp.watch('../assets/src/sass/**/*.scss', ['sass']);
     gulp.watch('../assets/src/css/*.css', ['cssmin']);
     gulp.watch('../assets/public/css/*.min.css', ['concatcss']);
 });
